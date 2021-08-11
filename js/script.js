2645ff1c7ef467083d484a2117565790777f122d
jordan <JordansGit@users.noreply.github.com>
2021-08-11 13:37:41 +0100

    changed bookmark button size and svg colour, added javascript for bookmark button

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var bookmarkBtn = document.querySelector(".button-bookmark");
+var bookmarkText = document.getElementById("bookmark-text"); 
+var bookmarkPath = document.querySelector(".bookmark-svg > g > path");
+var bookmarkCircle = document.querySelector(".bookmark-svg > g > circle");
+var check = false; 
+
+
+bookmarkBtn.onclick = function() {
+  if (check == false) {
+    bookmarkText.innerHTML = "Bookmarked";
+    bookmarkBtn.style.width = "215px";
+    bookmarkPath.style.fill = "hsl(0, 0%, 100%)";
+    bookmarkBtn.style.backgroundColor = "hsl(176, 50%, 94%)";  
+    check = true; 
+  } else {
+    bookmarkText.innerHTML = "Bookmark";
+    bookmarkBtn.style.width = "192px";
+    bookmarkPath.style.fill = "hsl(176, 50%, 47%)";
+    bookmarkBtn.style.backgroundColor = "hsl(176, 50%, 90%)";  
+    check = false; 
+  }
+}
+
+/* 
+on active: 
+ -> change text from bookmark -> bookmarked. **DONE. 
+ -> change the width of .button-bookmark to 215px. 
+
+ -> change the 'path' from medium grey to white / w.e the section colour is. 
+ -> change the circle colour of the button to a lighter shade of same colour. 
+
+
+*/ 
