5ac24dc957cdfedb573bcdecdc49d2ec967441c9
jordan <JordansGit@users.noreply.github.com>
2021-08-12 15:03:39 +0100

    more work on the button + started creating a modal

--- js/script.js
+++ js/script.js
@@ -35,3 +35,22 @@ on active:
 
 
 */ 
+
+var modal = document.getElementById("modal"); // Get the modal
+var btn = document.querySelector(".button-one"); // Get button that opens the modal
+var close = document.querySelector(".close"); // Get div that closes the modal
+
+btn.onclick = function() { // When the user clicks on the button, open the modal
+  modal.style.display = "block";
+}
+
+close.onclick = function() { // When the user clicks on <span> (x), close the modal
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
